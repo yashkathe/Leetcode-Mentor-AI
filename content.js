bda6c9f48f83953cd007b336830b1ff32fcb2637
Yash Kathe <yashkathe123@gmail.com>
2024-08-30 23:16:26 +0530

    created button to get hints. captured text content from dom elements for llm api

--- content.js
+++ content.js
@@ -1,2 +1,89 @@
-// content.js
 console.log('Content script loaded');
+
+// get Element and its Text Content
+const getElement = (selector, messageID) => {
+
+    const waitForElement = () => {
+
+        const element = document.querySelector(selector);
+
+        if(element) {
+
+            // access header
+            let content = element.textContent;
+
+            // send message to PoPUP
+            browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+
+                if(message.type === messageID) {
+                    sendResponse({ title: content });
+                }
+
+            });
+
+        } else {
+            setTimeout(waitForElement, 100);
+        }
+
+    };
+
+    waitForElement();
+
+};
+
+// get problem title
+getElement('a.cursor-text', 'GET-TITLE');
+
+// get content for API
+// get Element and its Text Content
+const getDataForApi = (selectors, messageID) => {
+
+    const waitForElement = () => {
+
+        let [ problemSelector, languageSelector, solutionSelector ] = selectors;
+
+        const problemEle = document.querySelector(problemSelector)
+        const languageEle = document.querySelector(languageSelector)
+        const solutionEle = document.querySelector(solutionSelector)
+
+
+        if(problemEle && languageEle && solutionEle) {
+
+            // access header
+            let problemContent = problemEle.textContent;
+            let languageContent = languageEle.textContent;
+            let solutionContent = solutionEle.textContent;
+
+
+            // send message to PoPUP
+            browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+
+                if(message.type === messageID) {
+                    sendResponse({
+                        problem: problemContent,
+                        language: languageContent,
+                        solution: solutionContent
+                    });
+                }
+
+            });
+
+        } else {
+            setTimeout(waitForElement, 100);
+        }
+
+    };
+
+    waitForElement();
+
+};
+
+// get content for api
+getDataForApi([
+    '.elfjS',
+    '.px-1\\.5',
+    'div.flex-1:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)'
+]
+    , 'GET-API-DATA');
+
+            
--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
+console.log('pop up script loaded');
+
 // check if it matches the website
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
     browser.tabs
         .query({ active: true, currentWindow: true })
         .then((tabs) => {
@@ -8,20 +10,102 @@ document.addEventListener("DOMContentLoaded", function() {
             const currentURL = activeTab.url;
 
             // get access to elements
-            const statusMessageElement = document.querySelector("#status-header");
-            const statusEmoji = document.querySelector("#status-para");
+            const statusHeader = document.querySelector("#status-header");
+            const statusPara = document.querySelector("#status-para");
 
             // change the text content of a website
-            if(statusMessageElement) {
+            if(statusHeader) {
                 if(!currentURL.includes("leetcode.com/problems/")) {
-                    statusMessageElement.textContent = " Offsite ⚠️";
-                    statusEmoji.innerHTML =
-                        "This extension currently only support LeetCode <br> Support for other websites will be added later";
+
+                    // If the URL doesn't match LeetCode
+                    if(statusHeader) {
+                        statusHeader.textContent = "Offsite ⚠️";
+                        statusPara.innerHTML = "This extension currently only supports LeetCode.<br>Support for other websites will be added later.";
+                    }
                 }
             }
         })
         .catch((error) => {
-            statusMessageElement.textContent = " Dev Error 🔧";
-            statusEmoji.textContent = error;
+            const statusHeader = document.querySelector("#status-header");
+            const statusPara = document.querySelector("#status-para");
+
+            statusHeader.textContent = " Dev Error 🔧";
+            statusPara.textContent = error;
+        });
+});
+
+
+// set the title
+document.addEventListener('DOMContentLoaded', () => {
+
+    browser.tabs
+        .query({ active: true, currentWindow: true })
+        .then((tabs) => {
+            // get access to tabs
+            const activeTab = tabs[ 0 ];
+            const currentURL = activeTab.url;
+
+            // get access to elements
+            const mainDiv = document.querySelector('#active-leetcode');
+            const titleHeader = document.querySelector("#problem-title");
+            const statusDiv = document.querySelector('#status-div');
+            const spinner = document.querySelector('.loader');
+            const hintButton = document.querySelector('#hint-btn');
+
+            if(currentURL.includes("leetcode.com/problems/")) {
+
+                hintButton.style.display = 'block';
+                mainDiv.style.display = 'flex';
+                statusDiv.style.display = 'none';
+
+                browser.tabs.sendMessage(activeTab.id, { type: "GET-TITLE" }).then((response) => {
+                    if(response && response.title) {
+                        titleHeader.textContent = response.title + ' 🚀';
+                    } else {
+                        titleHeader.textContent = "Title not found";
+                        statusPara.textContent = "Could not fetch the problem title.";
+                    }
+                }).catch((error) => {
+                    titleHeader.textContent = 'Loading ⌛';
+                    spinner.style.display = 'inline-block';
+                    statusDiv.style.display = 'none';
+                    hintButton.style.display = 'none';
+                });
+            }
+
+        });
+
+});
+
+// fetch hints from API
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    browser.tabs
+        .query({ active: true, currentWindow: true })
+        .then((tabs) => {
+            // get access to tabs
+            const activeTab = tabs[ 0 ];
+
+            const hintButton = document.querySelector('#hint-btn');
+            hintButton.addEventListener('click', () => {
+
+                // get access to elements
+                const readme = document.querySelector('#readme');
+                const spinner = document.querySelector('.loader');
+
+                // fetch api
+                spinner.style.display = 'inline-block';
+
+                browser.tabs.sendMessage(activeTab.id, { type: "GET-API-DATA" }).then((response) => {
+                    if(response) {
+                        console.log(response);
+                    }
+                }).catch((error) => {
+                    console.log(error);
+                });
+
+                spinner.style.display = 'none';
+            });
         });
 });
